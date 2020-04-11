2b8497a8e62dde1532141cd50e6906f5316b6ad5
Elliot Young <efonov@Elliots-MacBook-Pro.local>
2020-04-11 14:48:56 -0400

    testing form pt 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,19 +38,26 @@ export const Layout = ({ children, data }) => {
                     </div>
                 </div>
                 <motion.nav ref={containerRef} initial={false} animate={isOpen ? "open" : "closed"}>
-                    <div className="desktop-nav"><Navigation toggle={() => toggleOpen()} /></div>
-                    <div className="mobile-nav"><MobileNav toggle={() => toggleOpen()} /></div>
+                    <div className="desktop-nav">
+                        <Navigation toggle={() => toggleOpen()} /></div>
+                    <div className="mobile-nav">
+                        <MobileNav toggle={() => toggleOpen()} /></div>
                 </motion.nav>
                 <section class="page-section footer">
                     <div class="footer-text">
                         <div id="w-node-552eb64915d6-b64915d4" class="grid-container">
-                            <div class="title">Wheaton&#x27;s Independent<br />Undergraduate <br />Academic Journal</div>
+                            <div class="title">Wheaton&#x27;s Independent
+                    <br />Undergraduate
+                    <br />Academic Journal</div>
                             <div class="form-block w-form">
                                 <form id="wf-form-Newsletter-Form" name="wf-form-Newsletter-Form" data-name="Newsletter Form" method="post" netlify-honeypot="bot-field" data-netlify="true" class="form">
                                     <div href="#support-us" class="link _2">
                                         <div class="text-block-5">Get The Pub newsletter delivered right to your inbox.</div>
                                     </div>
-                                    <div class="div-block-2"><input type="hidden" class="text-field w-input" maxlength="256" name="bot-field" data-name="Email 2" placeholder="Enter your email" id="email-2" required="" /><button type="submit" class="simple-button fill w-button" >Sign Up</button></div>
+                                    <div class="div-block-2">
+                                        <input type="email" class="text-field w-input" maxlength="256" name="bot-field" data-name="Email 2" placeholder="Enter your email" id="email-2" required="" />
+                                        <button type="submit" class="simple-button fill w-button">Sign Up</button>
+                                    </div>
                                 </form>
                                 <div class="w-form-done">
                                     <div>Thank you! Your submission has been received!</div>
@@ -61,11 +68,34 @@ export const Layout = ({ children, data }) => {
                             </div>
                         </div>
                         <div id="w-node-552eb64915eb-b64915d4" class="footer-column">
-                            <div class="footer-column-name">Issues</div><StyledLink to={`issues/spring-2020`}><div class="footer-link">Latest Issue</div></StyledLink><StyledLink to="/issues"><div class="footer-link">Archive</div></StyledLink></div>
+                            <div class="footer-column-name">Issues</div>
+                            <StyledLink to={`issues/spring-2020`}>
+                                <div class="footer-link">Latest Issue</div>
+                            </StyledLink>
+                            <StyledLink to="/issues">
+                                <div class="footer-link">Archive</div>
+                            </StyledLink>
+                        </div>
                         <div id="w-node-552eb64915f2-b64915d4" class="footer-column">
-                            <div class="footer-column-name">Information</div><StyledLink to="/about"><div class="footer-link">About</div></StyledLink><StyledLink to="/staff"><div class="footer-link">Staff</div></StyledLink><StyledLink to="/blog"><div class="footer-link">Blog</div></StyledLink></div>
+                            <div class="footer-column-name">Information</div>
+                            <StyledLink to="/about">
+                                <div class="footer-link">About</div>
+                            </StyledLink>
+                            <StyledLink to="/staff">
+                                <div class="footer-link">Staff</div>
+                            </StyledLink>
+                            <StyledLink to="/blog">
+                                <div class="footer-link">Blog</div>
+                            </StyledLink>
+                        </div>
                         <div class="footer-column">
-                            <div class="footer-column-name">Contact</div><StyledLink to="/submissions"><div class="footer-link">Submit Work</div></StyledLink><StyledLink to="/help-out"><div class="footer-link">Support The Pub</div></StyledLink><a href="https://www.instagram.com/wheatonpub/" class="footer-link">Instagram</a><a href="mailto:wheatonpub@gmail.com" class="footer-link">Email</a></div>
+                            <div class="footer-column-name">Contact</div>
+                            <StyledLink to="/submissions">
+                                <div class="footer-link">Submit Work</div>
+                            </StyledLink>
+                            <StyledLink to="/help-out">
+                                <div class="footer-link">Support The Pub</div>
+                            </StyledLink><a href="https://www.instagram.com/wheatonpub/" class="footer-link">Instagram</a><a href="mailto:wheatonpub@gmail.com" class="footer-link">Email</a></div>
                         <div id="w-node-552eb649160c-b64915d4" class="final-things-copy">
                             <div class="small-text">Copyright © 2020 The Pub </div>
                             <div class="text-block-9">Website by <a href="http://efonov.com" class="link-2">@efonov</a></div>
