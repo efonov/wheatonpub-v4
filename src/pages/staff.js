e192d8ba837e0ac8a02ff027d12dda374532648d
Elliot Young <efonov@Elliots-MacBook-Pro.local>
2020-04-10 21:47:57 -0400

    found the typo 🥺

--- src/pages/staff.js
+++ src/pages/staff.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
-import { Layout } from "../components/Layout"
+import { Layout } from "../components/layout"
 import { motion } from "framer-motion"
 import { withPrefix, graphql } from "gatsby"
 import Img from 'gatsby-image'
