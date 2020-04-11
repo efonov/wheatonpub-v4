d6fe274427378a3b31e86a182748bf75e28d4fc7
Elliot Young <efonov@Elliots-MacBook-Pro.local>
2020-04-10 22:03:04 -0400

    changed css order

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ import { Layout } from "../components/layout"
 import { motion, useViewportScroll, useTransform } from "framer-motion"
 import { withPrefix } from "gatsby"
 
-import '../css/webflow.css'
-import '../css/normalize.css'
 import '../css/imported-styles.css'
 import '../css/custom-styles.css'
 
+import '../css/webflow.css'
+import '../css/normalize.css'
 
 const { scrollYProgress } = useViewportScroll();
 const HomePage = () => (
